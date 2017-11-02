911850a75f432a34570d5039ae9b20063be9c7c8
Paul Solt <PaulSolt@gmail.com>
2017-11-02 09:27:40 -0400

    Starting Project for Tip Calculator Auto Layout.

--- Tip Calculator UI/ViewController.swift
+++ Tip Calculator UI/ViewController.swift
@@ -8,18 +8,111 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // Outlets
+    @IBOutlet weak var tip1Label: UILabel!
+    @IBOutlet weak var tip2Label: UILabel!
+    @IBOutlet weak var tip3Label: UILabel!
+    @IBOutlet weak var billTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        billTextField.delegate = self
+        
+        // Listen for keyboard events
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardDidChange(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardDidChange(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardDidChange(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Actions
+    @IBAction func calculateTipButtonPressed(_ sender: Any) {
+        print("Calculate Tip!")
+
+        hideKeyboard()
+        calculateAllTips()
     }
-
-
+    
+    // UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        calculateAllTips()
+        return true
+    }
+    
+    
+    // Methods
+    
+    func calculateAllTips() {
+        // Get the subtotal as a number
+        guard let subtotal = convertCurrencyToDouble(input: billTextField.text!) else {
+            print("Error invalid number: \(billTextField.text!)")
+            return
+        }
+        
+        print("The subtoal is: \(subtotal)")
+        
+        // Calculate tips
+        let tip1 = calculateTip(subtotal: subtotal, tipPercentage: 10.0)
+        let tip2 = calculateTip(subtotal: subtotal, tipPercentage: 15.0)
+        let tip3 = calculateTip(subtotal: subtotal, tipPercentage: 20.0)
+        
+        // Update the UI
+        tip1Label.text = convertDoubleToCurrency(amount: tip1)
+        tip2Label.text = convertDoubleToCurrency(amount: tip2)
+        tip3Label.text = convertDoubleToCurrency(amount: tip3)
+    }
+    
+    /// - parameter tipPercentage: a value from 0 to 100
+    func calculateTip(subtotal: Double, tipPercentage: Double) -> Double {
+        return subtotal * (tipPercentage / 100.0)
+    }
+    
+    func convertCurrencyToDouble(input: String) -> Double? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+        
+        return numberFormatter.number(from: input)?.doubleValue
+    }
+    
+    func convertDoubleToCurrency(amount: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+        
+        return numberFormatter.string(from: NSNumber(value: amount))!
+    }
+    
+    func hideKeyboard() {
+        print("Hide keyboard")
+        billTextField.resignFirstResponder()
+    }
+    
+    @objc func keyboardDidChange(notification: Notification) {
+        print("keyboardDidChange \(notification.name)")
+        
+        // Actual keyboard height
+        guard let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        // Show or Hide
+        
+        if notification.name == Notification.Name.UIKeyboardWillShow ||
+            notification.name == Notification.Name.UIKeyboardWillChangeFrame {
+            view.frame.origin.y = -keyboardRect.height
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    
 }
 
